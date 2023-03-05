18b5ee8996656c609e76cf342b52f392101fc662
Ahui <842629581@qq.com>
2023-03-05 20:19:30 +0800

    feat:new

--- pages/order/order.js
+++ pages/order/order.js
@@ -41,7 +41,7 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow() {
-        console.log('gitfeat')
+        console.log('gitfeatnew')
         if(app.globalData.token==null){
             app.globalData.token=1
             wx.showModal({
